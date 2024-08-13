cc2be9f2a8880eaa7528a2905338c0dc945ec641
CollinTheDev <collin.entringer@gmail.com>
2024-08-12 21:26:59 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSender
