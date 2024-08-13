b256703f2db591bcb8b48625f4cb9d8ac8188b8c
CollinTheDev <collin.entringer@gmail.com>
2024-08-12 21:41:06 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,20 @@
-// Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBfd3UQO7owT-WRlLQY1cydZSNhZItjLe4",
+  authDomain: "not-mine-cf89a.firebaseapp.com",
+  projectId: "not-mine-cf89a",
+  storageBucket: "not-mine-cf89a.appspot.com",
+  messagingSenderId: "483691030126",
+  appId: "1:483691030126:web:9542c3ee53e141b140e87f"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
 const db = firebase.firestore();
